7fd3d1c59e68e15660990633cb40e893e17cea64
Peter Varga <pkvarg@yahoo.se>
2022-10-22 15:52:57 +0200

    28

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk' // I still add this for my reference so I know thunk middleware is added
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
+
+const reducer = combineReducers({
+  productList: productListReducer,
+})
 
 const store = configureStore({
   reducer: {},
