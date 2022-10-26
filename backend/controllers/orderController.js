5cb511c7c1ac86c884103b1e8208d03db083bb63
Peter Varga <pkvarg@yahoo.se>
2022-10-26 17:22:51 +0200

    58

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -34,4 +34,23 @@ const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder)
   }
 })
-export { addOrderItems }
+
+// @desc Get orderd by ID
+// @desc GET /api/orders/:id
+// @access Private
+
+const getOrderByid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderByid }
