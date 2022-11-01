971c4178385a74079f2ef9efeac0fcb4c351c669
Peter Varga <pkvarg@yahoo.se>
2022-11-01 14:40:36 +0100

    pre deploy 3 FormContainer ok

--- frontend/src/components/formContainer.js
+++ frontend/src/components/formContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
-const formContainer = ({ children }) => {
+const FormContainer = ({ children }) => {
   return (
     <Container>
       <Row className='justify-content-md-center'>
@@ -13,4 +13,4 @@ const formContainer = ({ children }) => {
   )
 }
 
-export default formContainer
+export default FormContainer
